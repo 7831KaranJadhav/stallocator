2ca88ed4ec50f11fea5fff39b72e1b2ea8bbaef7
unknown <kj1595@gmaiil.com>
2024-08-14 23:14:42 +0530

    Backend Commit

---
+++ src/main/java/com/Stallocator/StallocatorApplication.java
@@ -0,0 +1,13 @@
+package com.Stallocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StallocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StallocatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Stallocator/StallocatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Stallocator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StallocatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
